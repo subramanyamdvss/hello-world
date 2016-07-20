37a3a69ecbeb27159663e426dfb015ea358f539e
subramanyamdvss <subramanyamdvss@gmail.com>
2016-07-20 20:57:24 +0530

    Add files via upload

---
+++ my coding evironment.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#include <strings.h>
+using namespace std;
+
+typedef  long long lli;
+typedef vector< long long int > vi;
+typedef  double d;
+typedef  pair<lli,pair<lli,lli> >  pipii;
+#define bg begin()
+#define rbg rbegin()
+#define ren rend()
+#define en end()
+#define f first
+#define s second
+#define For(ii,aa,bb) for(lli ii=aa;ii<=bb;++ii)
+#define Rof(ii,aa,bb) for(lli ii=aa;ii>=bb;--ii)
+#define pb push_back
+#define minf(a,b,c) min(min(a,b),c)
+#define maxf(a,b,c) max(max(a,b),c)
+#define mp make_pair
+#define error(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+
+vector<string> split(const string& s, char c) {
+	vector<string> v;
+	stringstream ss(s);
+	string x;
+	while (getline(ss, x, c))
+		v.emplace_back(x);
+	return move(v);
+}
+
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+	cerr << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << '\n';
+	err(++it, args...);
+}
