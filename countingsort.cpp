6222f973de0f9249854edd856037c35b263d1153
subramanyamdvss <subramanyamdvss@gmail.com>
2016-08-31 09:19:18 +0530

    Create countingsort.cpp

---
+++ countingsort.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef  long long lli;
+typedef vector< long long int > vi;
+typedef  double d;
+typedef  pair<lli,pair<lli,lli> >  pipii;
+#define bg begin()
+#define rbg rbegin()
+#define ren rend()
+#define en end()
+#define f first
+#define s second
+#define For(ii,aa,bb) for(lli ii=aa;ii<=bb;++ii)
+#define Rof(ii,aa,bb) for(lli ii=aa;ii>=bb;--ii)
+#define pb push_back
+#define minf(a,b,c) min(min(a,b),c)
+#define maxf(a,b,c) max(max(a,b),c)
+#define mp make_pair
+#define error(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+
+vector<string> split(const string& s, char c) {
+	vector<string> v;
+	stringstream ss(s);
+	string x;
+	while (getline(ss, x, c))
+		v.emplace_back(x);
+	return move(v);
+}
+
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+	cerr << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << '\n';
+	err(++it, args...);
+}
+/// ********* debug template bt Bidhan Roy *********
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+
+void countingsort(lli a[],lli c[],lli n,lli mi,lli  ma){
+    lli b[ma+1];
+    For(i,0,ma){
+        b[i]=0;
+    }
+    //error(ma);
+    For(i,0,n-1){
+        b[a[i]]+=1;
+    }
+    For(i,1,ma){
+        b[i]+=b[i-1];
+    }
+    Rof(i,n-1,0){
+        if(b[a[i]]>0)
+            c[b[a[i]]]=a[i];
+        b[a[i]]--;
+    }
+    return;
+}
+
+
+
+
+
+int main(){
+  lli n;
+  cout<<"enter n and a values"<<endl;
+  cin>>n;
+  lli a[n];
+  lli c[n+1];
+  lli mi=1e9,ma=-1e9;
+  For(i,0,n-1){
+    cin>>a[i];
+    ma=max(ma,a[i]);
+    mi=min(mi,a[i]);
+  }
+
+  countingsort(a,c,n,mi,ma);
+  For(i,1,n ){
+    cout<<c[i]<<endl;
+  }
+  return 0;
+
+
+
+
+}
+
